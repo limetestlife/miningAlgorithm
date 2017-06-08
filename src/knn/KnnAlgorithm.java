ebd439f295601a8f3a57b1fbe9bf1b16f8b50361
limetestlife <568509350@qq.com>
2017-06-08 21:35:30 +0800

    数据挖掘算法KNN的java实现

---
+++ src/knn/KnnAlgorithm.java
@@ -0,0 +1,22 @@
+package knn;
+/**
+ * 
+ * @author lime
+ * @version 1.0 2017-6-8
+ * 该类是KNN近邻分类的主方法。在这个类中将实现具体的knn算法。
+ * 实现步骤：
+ * 	   1）计算已知类别数据集中的点与当前点之间的距离
+       2）按照距离递增次序排序
+       3）选取与当前距离最小的k个点
+       4）确定前k个点所在类别的出现频率
+       5）返回前k个点出现频率最高的类别作为当前点的预测分类
+ *
+ */
+public class KnnAlgorithm {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/knn/KnnClient.java
@@ -0,0 +1,17 @@
+package knn;
+/**
+ * KNN使用思路及实例
+ * @author lime
+ * 1.加载清洗好的数据
+ * 2.对比测试数据集，找到最好的参数K
+ * 
+ * 在面对大数据集时，可以高并发进行，也可以分布式集群进行
+ */
+public class KnnClient {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
